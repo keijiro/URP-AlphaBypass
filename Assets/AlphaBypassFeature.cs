aa7fea863d2f0e7961ef7737fbc2828350921b86
Keijiro Takahashi <keijiro@gmail.com>
2025-02-12 21:08:38 +0900

    Changed to use custom context data

--- Assets/AlphaBypassFeature.cs
+++ Assets/AlphaBypassFeature.cs
@@ -5,20 +5,19 @@ using UnityEngine.Rendering.RenderGraphModule.Util;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Experimental.Rendering;
 
+class AlphaData : ContextItem
+{
+    public TextureHandle Buffer { get; set; }
+    public override void Reset() => Buffer = TextureHandle.nullHandle;
+}
+
 sealed class SaveAlphaPass : ScriptableRenderPass
 {
     Material _material;
 
-    public TextureHandle Buffer { get; set; }
+    public SaveAlphaPass(Material material) => _material = material;
 
-    public SaveAlphaPass(Material material)
-    {
-        renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
-        _material = material;
-    }
-
-    public override void RecordRenderGraph(RenderGraph graph,
-                                           ContextContainer context)
+    public override void RecordRenderGraph(RenderGraph graph, ContextContainer context)
     {
         // Not supported: Back buffer source
         var resource = context.Get<UniversalResourceData>();
@@ -31,10 +30,15 @@ sealed class SaveAlphaPass : ScriptableRenderPass
         desc.colorFormat = GraphicsFormat.R8_UNorm;
         desc.clearBuffer = false;
         desc.depthBufferBits = 0;
-        Buffer = graph.CreateTexture(desc);
+        var buffer = graph.CreateTexture(desc);
+
+        // Custom context data for transferring the texture
+        var data = context.Create<AlphaData>();
+        data.Buffer = buffer;
 
         // Blit
-        var param = new RenderGraphUtils.BlitMaterialParameters(source, Buffer, _material, 0);
+        var param = new RenderGraphUtils.
+          BlitMaterialParameters(source, buffer, _material, 0);
         graph.AddBlitPass(param, passName: "Save Alpha");
     }
 }
@@ -42,25 +46,21 @@ sealed class SaveAlphaPass : ScriptableRenderPass
 sealed class LoadAlphaPass : ScriptableRenderPass
 {
     Material _material;
-    SaveAlphaPass _saver;
 
-    public LoadAlphaPass(Material material, SaveAlphaPass saver)
-    {
-        renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
-        _material = material;
-        _saver = saver;
-    }
+    public LoadAlphaPass(Material material) => _material = material;
 
-    public override void RecordRenderGraph(RenderGraph graph,
-                                           ContextContainer context)
+    public override void RecordRenderGraph(RenderGraph graph, ContextContainer context)
     {
         // Not supported: Back buffer source
         var resource = context.Get<UniversalResourceData>();
         if (resource.isActiveTargetBackBuffer) return;
 
+        // Alpha texture from the custom context data
+        var data = context.Get<AlphaData>();
+
         // Blit
-        var param = new RenderGraphUtils.BlitMaterialParameters
-          (_saver.Buffer, resource.activeColorTexture, _material, 1);
+        var param = new RenderGraphUtils.
+          BlitMaterialParameters(data.Buffer, resource.activeColorTexture, _material, 1);
         graph.AddBlitPass(param, passName: "Load Alpha");
     }
 }
@@ -70,13 +70,16 @@ public sealed class AlphaBypassFeature : ScriptableRendererFeature
     [SerializeField] Shader _shader = null;
 
     Material _material;
-    (SaveAlphaPass save, LoadAlphaPass load) _passes;
+    SaveAlphaPass _savePass;
+    LoadAlphaPass _loadPass;
 
     public override void Create()
     {
         _material = CoreUtils.CreateEngineMaterial(_shader);
-        _passes.save = new SaveAlphaPass(_material);
-        _passes.load = new LoadAlphaPass(_material, _passes.save);
+        _savePass = new SaveAlphaPass(_material);
+        _loadPass = new LoadAlphaPass(_material);
+        _savePass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        _loadPass.renderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
     }
 
     protected override void Dispose(bool disposing)
@@ -86,7 +89,7 @@ public sealed class AlphaBypassFeature : ScriptableRendererFeature
                                          ref RenderingData data)
     {
         if (data.cameraData.cameraType != CameraType.Game) return;
-        renderer.EnqueuePass(_passes.save);
-        renderer.EnqueuePass(_passes.load);
+        renderer.EnqueuePass(_savePass);
+        renderer.EnqueuePass(_loadPass);
     }
 }
